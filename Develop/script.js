4d087510e5113b023e24e8dab079dfe7123d186e
Mike Williams <williams.michael.256@outlook.com>
2022-03-06 11:24:05 -0600

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,24 @@ function writePassword() {
 
   passwordText.value = password;
 
+  var length = window.prompt("Enter a number from 8 to 128 for password length.");
+
+  var lowercase = false;
+    if (confirm("Do you want lowercase characters?") === true) {
+      lowercase = true
+    }
+  var uppercase = false;
+  if (confirm("Do you want uppercase characters?") === true) {
+    uppercase = true
+  }
+  var numeric = false;
+  if (confirm("Do you want numeric characters?") === true) {
+    numeric = true
+  }
+  var special = false;
+  if (confirm("Do you want special characters?") === true) {
+    special = true
+  }
 }
 
 // Add event listener to generate button
